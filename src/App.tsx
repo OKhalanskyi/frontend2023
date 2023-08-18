26b25f4167126db2f776184c79b022c4db7e04e4
Oleh Khalanskyi <okhalanskyi@gmail.com>
2023-08-18 23:16:37 +0300

    webpack base configured (react added)

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './style.module.scss';
+import './index.scss'
+
+const App = () => {
+  return (
+    <div className="app">
+      sdsd
+      <h2 className={styles.cl}>
+        18.08 webpack react
+      </h2>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export const f1 = ():number => {
+	console.log('f1')
+
+	return 345;
+}
