c9660b6d477a8fd6ca31cbdd8869a6e5bc2a6241
Oleh Khalanskyi <okhalanskyi@gmail.com>
2023-08-19 14:39:23 +0300

    feat: router added, theme changer provided

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import styles from './style.module.scss';
-import './index.scss'
+import React, { Suspense } from 'react';
+import './styles/index.scss'
+import { Link, Route, Routes } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/Main.page.async';
+import { AboutPageAsync } from './pages/AboutPage/About.page.async';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
-      sdsd
-      <h2 className={styles.cl}>
-        18.08 webpack react
-      </h2>
+    <div className={`app ${theme}`}>
+      <Link to="/about" >about</Link>
+      <Link to="/" >main</Link>
+      <button onClick={toggleTheme}>theme</button>
+      <Suspense fallback={<h1>Loading</h1>} >
+        <Routes>
+          <Route path={'/'} element={<MainPageAsync />} />
+          <Route path={'/about'} element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
